feb39fb42acde26f444e4a5ccb6482fada778bed
nafees nazik <nafeesnazik21@gmail.com>
2023-10-27 17:03:00 +0530

    feat: add permission check

--- backend/src/controllers/v3/secretsRemainderController.ts
+++ backend/src/controllers/v3/secretsRemainderController.ts
@@ -1,10 +1,15 @@
 import * as reqValidator from "../../validation/secretsRemainders";
 import { Request, Response } from "express";
-import { ProjectPermissionActions } from "../../ee/services/ProjectRoleService";
+import {
+  ProjectPermissionActions,
+  ProjectPermissionSub,
+  getUserProjectPermissions
+} from "../../ee/services/ProjectRoleService";
 import { checkSecretsPermission } from "../../helpers";
 import { validateRequest } from "../../helpers/validation";
 import { Types } from "mongoose";
 import { SecretRemainderService } from "../../services/SecretRemainderService";
+import { ForbiddenError } from "@casl/ability";
 
 /**
  * create secret remainder with name [secretName]
@@ -17,13 +22,11 @@ export const createRemainder = async (req: Request, res: Response) => {
     params: { secretName }
   } = await validateRequest(reqValidator.CreateSecretRemainder, req);
 
-  await checkSecretsPermission({
-    authData: req.authData,
-    workspaceId,
-    environment,
-    secretPath,
-    secretAction: ProjectPermissionActions.Create
-  });
+  const { permission } = await getUserProjectPermissions(req.user._id, workspaceId);
+  ForbiddenError.from(permission).throwUnlessCan(
+    ProjectPermissionActions.Create,
+    ProjectPermissionSub.SecretRemainder
+  );
 
   const secret = await SecretRemainderService.createSecretRemainder({
     environment,
@@ -44,6 +47,12 @@ export const deleteRemainder = async (req: Request, res: Response) => {
     body: { environment, workspaceId, secretPath },
     params: { secretName }
   } = await validateRequest(reqValidator.DeleteSecretRemainder, req);
+
+  const { permission } = await getUserProjectPermissions(req.user._id, workspaceId);
+  ForbiddenError.from(permission).throwUnlessCan(
+    ProjectPermissionActions.Delete,
+    ProjectPermissionSub.SecretRemainder
+  );
 
   await checkSecretsPermission({
     authData: req.authData,
@@ -71,6 +80,12 @@ export const updateRemainder = async (req: Request, res: Response) => {
     body: { environment, workspaceId, secretPath, secretRemainder },
     params: { secretName }
   } = await validateRequest(reqValidator.UpdateSecretRemainder, req);
+
+  const { permission } = await getUserProjectPermissions(req.user._id, workspaceId);
+  ForbiddenError.from(permission).throwUnlessCan(
+    ProjectPermissionActions.Edit,
+    ProjectPermissionSub.SecretRemainder
+  );
 
   const secret = await SecretRemainderService.updateSecretRemainder({
     environment,
